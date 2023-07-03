77868bd55f4d676a36226d389fc666ae342cc898
CHRISTENA EHAB <kokoehab1999@gmail.com>
2023-07-03 07:08:31 -0700

    finished

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,9 +11,9 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int t, r;
 
-	for (t = 0; s[t] != '\0'; i++)
+	for (t = 0; s[t] != '\0'; t++)
 	{
-		for (r = 0; accept[r] != s[i]; r++)
+		for (r = 0; accept[r] != s[t]; r++)
 		{
 			if (accept[r] == '\0')
 				return (t);
