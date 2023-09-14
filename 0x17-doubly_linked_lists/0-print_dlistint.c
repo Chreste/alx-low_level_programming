e936daf8183dc28777fa4c136c4481543b40e5ea
CHRISTENA EHAB <kokoehab1999@gmail.com>
2023-09-14 03:55:13 -0700

    finish

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,20 +8,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int x = 0;
+	size_t x = 0;
 
-	if (h == NULL)
-		return (x);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		x++;
 		h = h->next;
+		x++;
 	}
-
 	return (x);
 }
