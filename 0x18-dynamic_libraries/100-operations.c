e2beedf775964afe4e7b19380ef8224732237456
CHRISTENA EHAB <kokoehab1999@gmail.com>
2023-09-18 05:16:45 -0700

    finish

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int add(int i, int x)
+{
+	return i + x;
+}
+
+int sub(int i, int x)
+{
+	return i - x;
+}
+
+int mul(int i, int x)
+{
+	return i * x;
+}
+
+int div(int i, int x)
+{
+	return i / x;
+}
+
+int mod(int i, int x)
+{
+	return i % x;
+}
